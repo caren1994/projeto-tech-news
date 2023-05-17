a33d7da350b8994c300c0337a98dc2290fadf33f
caren <carenoliveirasilvapontes@gmail.com>
2023-05-17 18:46:44 -0300

    feat: req 7,8,9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,27 @@
+from tech_news.database import db
+from datetime import datetime
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = db.news.find({"title": {"$regex": title, "$options": "i"}})
+    return [(new["title"], new["url"]) for new in news]
 
 
+# o parametro options i significa "case-insensitive"
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date_iso = datetime.strptime(date, "%Y-%m-%d")
+        response = db.news.find({"timestamp": date_iso.strftime("%d/%m/%Y")})
+    except Exception:
+        raise ValueError("Data inválida")
+    return [(new["title"], new["url"]) for new in response]
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    response = db.news.find(
+        {"category": {"$regex": category, "$options": "i"}}
+    )
+    return [(new["title"], new["url"]) for new in response]
